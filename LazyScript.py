4ab9735a6bd9cabff7ca92174049776e0522952a
Suredsi Ulpada <78768764+cyberdesu@users.noreply.github.com>
2023-12-11 20:54:29 +0700

    Revert "release 1.0"
    This reverts commit 9a9a312b20bf40b9c38951fca3ab0d8b3593d3c7.

--- LazyScript.py
+++ LazyScript.py
@@ -71,96 +71,69 @@ def submitForm(username, password):
     
     response = session.get(url,headers=headers,cookies=cookies)
     soup = BeautifulSoup(response.text, "html.parser")
-    #endpoint = soup.find("a", {"class": "btn btn-info"})
-    #endpoint_value = endpoint.get("href")
-    #print(endpoint_value)
-    elements = soup.find_all("a", {"class": "btn btn-info"})
-
-    # Buat daftar untuk menyimpan nilai href dari setiap elemen
-    href_list = []
-
-    # Loop melalui setiap elemen dan tambahkan nilai href ke dalam daftar
-    for element in elements:
-        href_value = element.get("href")
-        href_list.append(href_value)
-
-    # Tampilkan daftar nilai href
-    for i, href in enumerate(href_list, start=1):
-        print(f"{i}. {href}")
-
-    # Buat input pilihan
-    selected_option = int(input("Pilih nomor opsi: "))
-
-    # Lakukan sesuatu dengan opsi yang dipilih (misalnya, buka URL)
-    selected_url = href_list[selected_option - 1]
-    print(f"Anda memilih opsi {selected_option}: {selected_url}")
-    response = session.get("https://studentportal.ipb.ac.id"+selected_url,headers=headers,cookies=cookies)
+    endpoint = soup.find("a", {"class": "btn btn-info"})
+    endpoint_value = endpoint.get("href")
+    
+    response = session.get("https://studentportal.ipb.ac.id"+endpoint_value,headers=headers,cookies=cookies)
     soup = BeautifulSoup(response.text, "html.parser")
     endpoint = soup.find("a", {"class": "btn btn-default btn-tool"})
     endpoint_value = endpoint.get("onclick")
     endpoint_value = endpoint_value.split("'")[1]
 
     # Set the start and end dates
-    start_date = datetime(2023, 8, 7)
-    end_date = datetime(2023, 8, 10) # hanya dikirim senin-jumat aja
+    start_date = datetime(2023, 8, 2)
+    end_date = datetime(2023, 8, 4)
 
     # Array of activities
     kegiatan_array = [
         "Pembagian Jobdesc dari pihak kantor",
         "Mencari Referensi data untuk project yang akan dibuat",
         "mencari referensi terkait mikrotik",
-        "belajar dasar front end di dicoding"
         # Add more activities as needed
     ]
     foto_kegiatan_array = [
         "1.jpg",
-        "2.jpeg",
-        "3.jpeg",
-        "4.jpeg"
+        "2.jpg",
+        "3.jpg",
         # Add more activities as needed
     ]
-    # Check if the lengths of all arrays are the same
-    if len(kegiatan_array) != len(foto_kegiatan_array):
-        print("Error: Lengths of 'kegiatan_array' and 'foto_kegiatan_array' are not the same.")
-        exit()
 
-    # Calculate the number of days
-    num_days = (end_date - start_date).days + 1
-
-    # Check if the lengths of tanggal, kegiatan_array, and foto_kegiatan_array are the same
-    if num_days != len(kegiatan_array):
-        print("Error: Lengths of 'tanggal', 'kegiatan_array', and 'foto_kegiatan_array' are not the same.")
-        exit()
-    # Iterate over the dates
+# Iterate over the dates
     current_date = start_date
     url_form = "https://studentportal.ipb.ac.id/Kegiatan/LogAktivitasKampusMerdeka/Tambah"
-
-    # Iterate over the activities and images
-    for i in range(len(kegiatan_array)):
-        kegiatan = kegiatan_array[i]
-        image = foto_kegiatan_array[i]
-
+    while current_date <= end_date:
         # Check if the current day is Monday to Friday
         if 0 <= current_date.weekday() <= 4:
+
+            response = session.get("https://studentportal.ipb.ac.id" + endpoint_value, headers=headers, cookies=cookies)
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            # Get aktivitasID value
+            AktivitasId = soup.find("input", {"name": "AktivitasId"})
+            AktivitasId_value = AktivitasId.get("value")
+            print(AktivitasId_value)
+
+            # get csrf token
+            csrf_token_form = soup.find("input", {"name": "__RequestVerificationToken"})
+            csrf_token_form_value = csrf_token_form.get("value")
+            print(csrf_token_form_value)
+
+            # Format the date
+            formatted_date = current_date.strftime("%d/%m/%Y")
+
+            # Select an activity from the array (cycling through activities)
+            kegiatan = kegiatan_array[current_date.day % len(kegiatan_array)]
+
+            # select image from array
+            image = foto_kegiatan_array[current_date.day % len(foto_kegiatan_array)]
+            image_folder = "Gambar"
+            image_filename = image
+
+            image_path = os.path.join(image_folder, image_filename)
+
             try:
-                image_folder = "Gambar"
-                image_filename = image
-                image_path = os.path.join(image_folder, image_filename)
-
                 with open(image_path, "rb") as image_file:
-                    response = session.get("https://studentportal.ipb.ac.id" + endpoint_value, headers=headers, cookies=cookies)
-                    soup = BeautifulSoup(response.text, "html.parser")
-                    AktivitasId = soup.find("input", {"name": "AktivitasId"})
-                    AktivitasId_value = AktivitasId.get("value")
-
-                    csrf_token_form = soup.find("input", {"name": "__RequestVerificationToken"})
-                    csrf_token_form_value = csrf_token_form.get("value")
-
-                    idDosen = soup.find("input", {"name": "ListDosenPembimbing[0].Key.PembimbingId"})
-                    idDosen_value = idDosen.get("value")
-
-                    formatted_date = current_date.strftime("%d/%m/%Y")
-
+                    # Create a dictionary with the form data
                     form_data = {
                         "Id": "",
                         "AktivitasId": AktivitasId_value,
@@ -168,21 +141,29 @@ def submitForm(username, password):
                         "Tmw": "8:00",
                         "Tsw": "16:00",
                         "JenisLogbookKegiatanKampusMerdekaId": "3",
-                        "ListDosenPembimbing[0].Key.PembimbingId": idDosen_value,
+                        "ListDosenPembimbing[0].Key.PembimbingId": "169043",
+                        "ListDosenPembimbing[1].Key.PembimbingId": "169146",
                         "IsLuring": "",
                         "Lokasi": "Jl. Jeruk Purut Dalam No.33, RT.6/RW.3, Cilandak Tim., Ps. Minggu, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12560",
                         "Keterangan": kegiatan,
+                        "FilePath": "",
                         "__RequestVerificationToken": csrf_token_form_value,
                         "ListDosenPembimbing[0].Value": "true",
+                        "ListDosenPembimbing[1].Value": "false",
                         "File": (image_filename, image_file, "image/jpeg"),
                     }
 
+                    # Create a multipart encoder
                     multipart_encoder = MultipartEncoder(fields=form_data)
+
+                    # Set the Content-Type header to the multipart encoding
                     headers_form["Content-Type"] = multipart_encoder.content_type
 
+                    # Send the HTTP request
                     response = session.post(url_form, headers=headers_form, data=multipart_encoder, cookies=cookies)
 
-                    print(f"Tanggal: {current_date.strftime('%A, %d %B %Y')}, Kegiatan: {kegiatan}, Status Code: {response.status_code}")
+                    # Print the result
+                    print(f"Date: {formatted_date}, Kegiatan: {kegiatan}, Status Code: {response.status_code}")
 
             except FileNotFoundError:
                 print(f"Error: File not found at {image_path}")
@@ -190,7 +171,8 @@ def submitForm(username, password):
             except Exception as e:
                 print(f"Error: {e}")
 
+        # Move to the next day
         current_date += timedelta(days=1)
 
-# Example usage
-submitForm("example_username", "example_pass") # change this to your username and password
+
+submitForm("username", "password")
